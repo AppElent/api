1bb3ff2fc9206e8ff3e7477d267841487c000e6a
Eric Jansen <ericjansen@live.nl>
2020-04-21 19:50:17 +0200

    deleteMeterstanden middleware

--- src/app/controllers/meterstanden.controller.ts
+++ src/app/controllers/meterstanden.controller.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 const router = express.Router();
 
+import { Op } from 'sequelize';
+import moment from 'moment';
 import Cache from '../modules/Cache';
 import SequelizeRoutes from '../modules/express-sequelize-routes';
 import { basicAuthentication } from '../middleware/authentication';
@@ -11,10 +13,29 @@ const longCache = new Cache();
 const shortCache = new Cache(300);
 const routes = new SequelizeRoutes({ idColumnName: 'id', userColumnName: 'userId', reqUserProperty: 'uid' });
 
+interface CustomRequest extends Request {
+    uid?: string;
+}
+
+const deleteMeterstanden = async (req: CustomRequest, res: Response, next: NextFunction): Promise<void> => {
+    await Meterstanden.destroy({
+        where: {
+            datetime: {
+                [Op.lt]: moment()
+                    .subtract(3, 'days')
+                    .startOf('day')
+                    .toDate(),
+            },
+            userId: req.uid,
+        },
+    });
+    next();
+};
+
 router.use(basicAuthentication);
 router.get('/:id', cacheMiddleware(longCache), asyncHandler(routes.get(Meterstanden)));
 router.get('/', cacheMiddleware(shortCache), asyncHandler(routes.list(Meterstanden)));
-router.post('/', asyncHandler(routes.create(Meterstanden)));
+router.post('/', deleteMeterstanden, asyncHandler(routes.create(Meterstanden)));
 router.put('/:id', asyncHandler(routes.update(Meterstanden)));
 router.delete('/:id', asyncHandler(routes.destroy(Meterstanden)));
 router.get('/:column/:value', cacheMiddleware(longCache), asyncHandler(routes.find(Meterstanden)));
